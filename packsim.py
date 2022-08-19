a3327d89e6da5e3dc809b93b8180b0d594b551b8
Maeve Cotton <ragnarokette@gmail.com>
2022-08-19 18:41:43 -0400

    added comments

--- packsim.py
+++ packsim.py
@@ -1,5 +1,9 @@
+#simulates the cash value of a pack of magic the gathering cards
+#this is an example generates a set that matches a draft pack of Streets of New Capenna in size and currently ignores the chance of foil cards
+#ideally you would have a list of everything possible in a given slot in the pack
 import random
 
+#generate an example set of cards
 #snc 101 commons, 80 uncommons, 60 rare, 20 mythic in new capenna
 commons = []
 for x in range(1, 102):
@@ -41,9 +45,13 @@ for x in range(1, 21):
     }
     mythics.append(card)   
 
+#add the commmons and mythics to the pack, without the possibility of duplicates
+#a real pack also would account for a spread of different color cards in the pack but the exact collation patterns aren't known so I just take cards at random
+#commons and uncommons are a negligible part of the value anyways, usually commmons are essentially worthless and there are a handful of uncommons worth money
 packcommmons = random.sample(commons, 10)
 packuncommons = random.sample(uncommons, 3)
 
+#mythics replace the rare in the pack in approximately one in six packs
 ismythic = random.randint(1,6)
 if ismythic == 6:
     packmythics = random.sample(mythics, 1)
